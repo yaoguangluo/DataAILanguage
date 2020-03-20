1de18a91307c382a5958f4f2b6c67f428189187c
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2020-03-20 14:43:27 +0800

    今天就到这里，最近事情多。20200320 罗瑶光

--- src/org/tinos/language/plorm/Select.java
+++ src/org/tinos/language/plorm/Select.java
@@ -1,5 +1,11 @@
 package org.tinos.language.plorm;
 public class Select{
+
+	//	String plsql= "setRoot:C:/DetaDB;" + 
+//			"baseName:ZYY;" + 
+//			"tableName:xybg:select;" +
+//			"condition:or:ID|<=|3000;";
+	
 	String PLSQL= "";
 	public String startAtRootDir(String rootAddress) {
 		PLSQL= "setRoot:"+ rootAddress+ ";";
@@ -13,6 +19,11 @@ public class Select{
 	
 	public String withTableSelect(String tableName) {
 		PLSQL+= "tableName:"+ tableName+ ":select;";
+		return PLSQL; 
+	}
+	//稍后我会把这个条件函数微分细化。
+	public String withCondition(String conditionType, String conditionText) {
+		PLSQL+= "condition:"+ conditionType+ ":"+ conditionText+ ";";
 		return PLSQL; 
 	}
 }
