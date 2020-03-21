843ef50145fd31c69d90a936ef9683ea155ec37b
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2020-03-21 09:12:13 +0800

    实现了select部分nosql 功能并论证成功，稍后进行系统化和优化。 20200321 罗瑶光

--- src/org/tinos/language/plorm/Select.java
+++ src/org/tinos/language/plorm/Select.java
@@ -1,29 +1,45 @@
 package org.tinos.language.plorm;
 public class Select{
-
-//	String plsql= "setRoot:C:/DetaDB;" + 
+//			String plsql= "setRoot:C:/DetaDB;" + 
 //			"baseName:ZYY;" + 
 //			"tableName:xybg:select;" +
 //			"condition:or:ID|<=|3000;";
-	
 	String PLSQL= "";
-	public String startAtRootDir(String rootAddress) {
-		PLSQL= "setRoot:"+ rootAddress+ ";";
+	public String getPLSQL() {
 		return PLSQL;
 	}
-	
-	public String withBaseName(String baseName) {
-		PLSQL+= "baseName:"+ baseName+ ";";
-		return PLSQL; 
+
+	public void setPLSQL(String pLSQL) {
+		PLSQL = pLSQL;
+	}
+
+	public Select startAtRootDir(String rootAddress) {
+		PLSQL= "setRoot:"+ rootAddress+ ";";
+		return this;
 	}
 	
-	public String withTableSelect(String tableName) {
+	public Select withBaseName(String baseName) {
+		PLSQL+= "baseName:"+ baseName+ ";";
+		return this;
+	}
+	
+	public Select withTableSelect(String tableName) {
 		PLSQL+= "tableName:"+ tableName+ ":select;";
-		return PLSQL; 
+		return this;
 	}
 	//稍后我会把这个条件函数微分细化。
-	public String withCondition(String conditionType, String conditionText) {
-		PLSQL+= "condition:"+ conditionType+ ":"+ conditionText+ ";";
-		return PLSQL; 
+	public Select withCondition(String conditionType) {
+		PLSQL+= "condition:"+ conditionType+ ":";
+		return this;
+	}
+
+	public Select let(String leftSet) {
+		PLSQL+= leftSet;
+		return this;
+	}
+
+	public Select lessAndEqualTo(String compareSet) {
+		PLSQL+= "|<=|"+compareSet+";";
+		return this;
 	}
 }
