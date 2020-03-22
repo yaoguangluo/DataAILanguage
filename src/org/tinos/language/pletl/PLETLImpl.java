48fb68a0b6afe077d8222a8037841430302c07fb
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2020-03-22 18:37:37 +0800

    //很好的将 《德塔socket plsql数据库》 和 《德塔 ETL》变成脑的记忆和计算中枢配合。 //别急，这个没有组合虽然没有自主意识，但是已经形成了VPCS计算神经的单株 锥形。20200322 罗瑶光

---
+++ src/org/tinos/language/pletl/PLETLImpl.java
@@ -0,0 +1,11 @@
+package org.tinos.language.pletl;
+import java.util.HashMap;
+public class PLETLImpl implements PLETLIntef{
+	@Override
+	public HashMap<String, Object> doNeroFlow(String documentFlowAddress, HashMap<String, Object> inputMap) {
+		//很好的将 《德塔socket plsql数据库》 和 《德塔 ETL》变成脑的记忆和计算中枢配合。
+		//别急，这个没有组合虽然没有自主意识，但是已经形成了VPCS计算神经的单株 锥形。20200322 罗瑶光 
+		return null;
+	}
+	//大爱如此
+}
---
+++ src/org/tinos/language/pletl/PLETLIntef.java
@@ -0,0 +1,5 @@
+package org.tinos.language.pletl;
+import java.util.HashMap;
+public interface PLETLIntef{
+	public HashMap<String, Object> doNeroFlow(String documentFlowAddress, HashMap<String, Object> inputMap);
+}
